af79b192e4c86537bc1b403cc8ce33132334d645
Scott Banwart <sbanwart@rogue-technology.com>
2017-02-07 20:44:27 -0500

    Add functionality to read an Excel spreadsheet, add columns adn data and write the result.

--- read-workbook-pandas.py
+++ read-workbook-pandas.py
@@ -1,14 +1,28 @@
 import pandas
 
-def print_excel():
-    workbook = pandas.read_excel('data.xlsx')
+def process_excel():
+    df = pandas.read_excel('data.xlsx')
     
-    print(workbook.columns)
+    print(df.columns)
     
     FORMAT = ['Name', 'Status']
     
-    selected = workbook[FORMAT]
+    selected = df[FORMAT]
     
     print(selected)
 
-print_excel()
+    print(df.iloc[0]['Name'])
+
+    df['Achievements'] = df.index
+    df['Completed'] = df.index
+
+    df.loc[1, 'Achievements'] = 20
+    df.loc[1, 'Completed'] = 30
+
+    writer = pandas.ExcelWriter('data.xlsx', engine='xlsxwriter')
+
+    df.to_excel(writer, sheet_name='Sheet1')
+
+    writer.save()
+
+process_excel()
